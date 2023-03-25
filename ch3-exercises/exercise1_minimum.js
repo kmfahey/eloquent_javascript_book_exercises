14c70aa0584f6a64eeb69f413d3a5d5e572385cd
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-25 16:38:57 -0700

    completed chapter 3 exercise 1: Minimum

---
+++ ch3-exercises/exercise1_minimum.js
@@ -0,0 +1,14 @@
+/* Minimum
+ * Chapter 2 introduced the standard function Math.min, which returns its
+ * smallest argument (see "Return Values" on page 27). We can build something
+ * like that now. Write a function min that takes two arguments and returns
+ * their minimum.
+ */
+
+function min(left_arg, right_arg) {
+    if (typeof left_arg != "number" && typeof left_arg != "bigint")
+        throw new Error("can't calculate minimum when first argument is non-numeric");
+    else if (typeof right_arg != "number" && typeof right_arg != "bigint")
+        throw new Error("can't calculate minimum when second argument is non-numeric");
+    return left_arg < right_arg ? left_arg : right_arg;
+}
