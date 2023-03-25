cde09700b56e852117b2d5b2dbb7ad38b21b3fe3
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-25 16:39:12 -0700

    completed chapter 3 exercise 1: Minimum

--- ch3-exercises/exercise1_minimum.js
+++ ch3-exercises/exercise1_minimum.js
@@ -1,4 +1,5 @@
 /* Minimum
+ * 
  * Chapter 2 introduced the standard function Math.min, which returns its
  * smallest argument (see "Return Values" on page 27). We can build something
  * like that now. Write a function min that takes two arguments and returns
