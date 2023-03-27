4ae969ecb832121f611c0fb871b449c065267a47
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-27 15:22:42 -0700

    added chapter 5 exercise files prior to completion

---
+++ ch5-exercises/exercise1_flattening.js
@@ -0,0 +1,7 @@
+/* 
+ * Flattening
+ *
+ * Use the reduce method in combination with the concat method to “flat-
+ * ten” an array of arrays into a single array that has all the elements of
+ * the original arrays.
+ */
