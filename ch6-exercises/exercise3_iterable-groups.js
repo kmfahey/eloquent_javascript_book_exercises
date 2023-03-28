a710f2f0788dd77b491066fccc87c19dfacac5f9
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-28 15:52:19 -0700

    completed chapter 6 exercise 3: Iterable Groups

--- ch6-exercises/exercise3_iterable-groups.js
+++ ch6-exercises/exercise3_iterable-groups.js
@@ -11,3 +11,66 @@
  * iterator behaves strangely when the group is modified during iteration.
  */
 
+
+class Group {
+    constructor() {
+        this.array = []
+    }
+
+    add(value) {
+        if (this.array.indexOf(value) == -1) {
+            this.array.push(value);
+        }
+    }
+
+    delete(value) {
+        let valueIndex = this.array.indexOf(value);
+        if (valueIndex == -1) return false;
+        this.array.splice(valueIndex, 1);
+        return true;
+    }
+
+    has(value) {
+        return this.array.indexOf(value) != -1;
+    }
+
+    static from(iterable) {
+        let group = new Group();
+        for (let value of iterable) {
+            group.add(value);
+        }
+        return group;
+    }
+
+    [Symbol.iterator]() {
+        let _index = -1;
+        return {
+            next: () => {
+                if (_index < this.array.length) {
+                    _index++;
+                }
+                return {
+                    // Falls off the end of the array in the done:true step but
+                    // that's nbd.
+                    value: this.array[_index],
+                    done: _index === this.array.length
+                }
+            }
+        }
+    }
+}
+
+let group = new Group();
+group.add('a');
+group.add('b');
+group.add('c');
+console.log(group);
+
+let group2 = Group.from(['a', 'b', 'c'])
+console.log(group2);
+
+for (let val of group2) {
+    console.log(val);
+    //group2.delete(val);
+    //Confirmed, it acts strangely.
+}
