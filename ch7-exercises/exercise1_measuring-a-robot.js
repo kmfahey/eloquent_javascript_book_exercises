8f939fd13bd659357dd787413765256fd5832a71
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-29 14:11:39 -0700

    completed chapter 7 exercise 1: Measuring a Robot

--- ch7-exercises/exercise1_measuring-a-robot.js
+++ ch7-exercises/exercise1_measuring-a-robot.js
@@ -16,3 +16,33 @@
 
 require('./eloqjs_ch7-robot_module.js');
 
+function epochSeconds() {
+    const now = new Date();
+    const utcMilllisecondsSinceEpoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
+    return utcMilllisecondsSinceEpoch / 1000;
+}
+
+function compareRobots(robot0, memory0, robot1, memory1) {
+    let times0 = [], times1 = [];
+    for (i = 0; i < 100; i++) {
+        let state = VillageState.random();
+        console.log(state.parcels);
+        let startTime0 = epochSeconds();
+        runRobot(state, robot0, memory0);
+        let endTime0 = epochSeconds();
+        let startTime1 = epochSeconds();
+        runRobot(state, robot1, memory1);
+        let endTime1 = epochSeconds();
+        times0.push(endTime0 - startTime0);
+        times1.push(endTime1 - startTime1);
+    }
+    sum0 = times0.reduce((a,b) => a + b, 0);
+    sum1 = times1.reduce((a,b) => a + b, 0);
+    average0 = sum0 / 100;
+    average1 = sum1 / 100;
+    fastest = average0 > average1 ? 0 : average1 > average0 ? 1 : -1;
+    return {times0, average0, times1, average1, fastest};
+}
+
+console.log(compareRobots(randomRobot, [], routeRobot, mailRoute));
+
