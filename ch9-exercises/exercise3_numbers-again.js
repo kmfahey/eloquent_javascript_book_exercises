9d9208e0863732b491d8d10b336bfa26a0ec18e7
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-30 12:14:50 -0700

    completed chapter 9 exercise 3: Numbers Again

--- ch9-exercises/exercise3_numbers-again.js
+++ ch9-exercises/exercise3_numbers-again.js
@@ -8,3 +8,11 @@
  * digits in front of or after the dot, but the number cannot be a dot alone.
  * That is, .5 and 5. are valid JavaScript numbers, but a lone dot isn’t.
  */
+
+JSstyleNumbers = [
+    "1", "121", "0.5", ".5", "0.", "1E+2", "1e+2", "1E-2", "1e-2", "121E+2",
+    "121e+2", "121E-2", "121e-2", "0.5E+2", "0.5e+2", "0.5E-2", "0.5e-2",
+    ".721E+2", ".721e+2", ".721E-2", ".721e-2", "721.E+2", "721.e+2", "721.E-2",
+    "721.e-2"
+]
+console.log(JSstyleNumbers.every(s => /[+-]?(\d+|\d+\.|\.\d+)([Ee][+-][0-9]+)?/.test(s)));
