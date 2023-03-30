3f4d429b7297470561af344da58a8a388f229a8d
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-03-30 14:40:21 -0700

    set up chapter 10 exercise script files and license file

---
+++ ch10-exercises/exercise2_roads-module.js
@@ -0,0 +1,10 @@
+/*
+ * Roads Module
+ *
+ * Write a CommonJS module, based on the example from Chapter 7, that contains
+ * the array of roads and exports the graph data structure representing them as
+ * roadGraph. It should depend on a module ./graph , which exports a function
+ * buildGraph that is used to build the graph. This function expects an array of
+ * two-element arrays (the start and end points of the roads).
+ */
+
