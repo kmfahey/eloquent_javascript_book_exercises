d198824a5c69ae68d66955be188a444c45ddf250
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-04-07 14:44:30 -0700

    completed chapter 15 exercise 3: Tabs

--- ch15-exercises/exercise3.js
+++ ch15-exercises/exercise3.js
@@ -1,5 +1,15 @@
 let tabSelected = null;
 let tabsById = new Map();
+let buttonRowDiv;
+const ADD_STRONG = true, REMOVE_STRONG = false;
+
+function buttonStrongTag(buttonNode, emph=null) {
+    if (emph === true) {
+        buttonNode.innerHTML = "<strong>" + buttonNode.getAttribute("data-tabname") + "</strong>";
+    } else if (emph === false) {
+        buttonNode.innerHTML = buttonNode.getAttribute("data-tabname");
+    }
+}
 
 function switchVisibleTab(event) {
     let buttonTabId = event.target.getAttribute("data-assoc-tab-id");
@@ -8,6 +18,14 @@ function switchVisibleTab(event) {
             tabNode.style.display = "block";
         } else {
             tabNode.style.display = "none";
+        }
+    }
+    for (let buttonNode of buttonRowDiv.childNodes) {
+        if (buttonNode.nodeType !== Node.ELEMENT_NODE) continue;
+        if (buttonNode === event.target) {
+            buttonStrongTag(buttonNode, ADD_STRONG);
+        } else {
+            buttonStrongTag(buttonNode, REMOVE_STRONG);
         }
     }
 }
@@ -31,12 +49,15 @@ function asTabs(tabsParentNode) {
     for (let [tabId, tabNode] of tabsById.entries()) {
         let buttonNode = document.createElement("button");
         buttonNode.innerHTML = tabNode.getAttribute("data-tabname");
+        buttonNode.setAttribute("data-tabname", tabNode.getAttribute("data-tabname"));
         buttonNode.setAttribute("data-assoc-tab-id", tabId);
         buttonNode.addEventListener("click", switchVisibleTab);
         buttonNodes.push(buttonNode);
     }
 
-    let buttonRowDiv = document.createElement("div");
+    buttonStrongTag(buttonNodes[0], ADD_STRONG);
+
+    buttonRowDiv = document.createElement("div");
     buttonRowDiv.style = document.createElement("style");
     buttonRowDiv.style.textAlign = "center";
     for (i = 0; i < buttonNodes.length; i++) {
