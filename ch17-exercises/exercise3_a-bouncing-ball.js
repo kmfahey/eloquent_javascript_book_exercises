30e1c43517a4cbb20f2f7aaf01936a1b48875085
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-04-16 15:38:08 -0700

    moved ex.3 js => html; first stage of soln

--- ch17-exercises/exercise3_a-bouncing-ball.js
+++
@@ -1,8 +0,0 @@
-/*
- * A Bouncing Ball
- *
- * Use the requestAnimationFrame technique that we saw in Chapter 14 and Chapter
- * 16 to draw a box with a bouncing ball in it. The ball moves at a constant
- * speed and bounces off the box’s sides when it hits them.
- */
-
