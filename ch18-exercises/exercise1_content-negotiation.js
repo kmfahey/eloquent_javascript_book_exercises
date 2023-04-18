72cb450427120aa1146e67a0a90ac5d2205a8986
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-04-18 12:32:45 -0700

    completed chapter 18, exercise 1: Content Negotiation

--- ch18-exercises/exercise1_content-negotiation.js
+++ ch18-exercises/exercise1_content-negotiation.js
@@ -20,48 +20,110 @@
  * which status code that produces.
  */
 
+const url = "https://eloquentjavascript.net/author";
 
-let contentTypes = ["text/plain", "text/html", "application/json"];
-let requestsArray = [];
-let allFetched = false;
-let outcomesDisplayed = false;
+let validContentTypes = ["text/plain", "text/html", "application/json"];
 let contentBodies = new Map();
-let url = "https://eloquentjavascript.net/author";
+let requestsPrm = [];
 
-for (let contentType of contentTypes) {
-    requestsArray.push(fetch(url, {headers: {Accept: contentType}}));
+/* 
+ * This program uses global flags and setInterval() callbacks to resynchronize
+ * the control flow after each dispatch of control to the resolution of a
+ * promise or promises.
+ *
+ * The asynchronous code stores any outcomes in the global
+ * data structure contentBodies, and sets a global flag when it's completed. In
+ * the global scope a setInterval() call repeatedly checks for that flag until
+ * it's true, whereupon the sync. code that was waiting on the async. code to
+ * complete runs.
+ *
+ * These are booleans used to signal completion of various asynchronous tasks.
+ */
+let firstPassFetched = false;
+let firstPassDisplayed = false;
+let secondPassFetched = false;
+
+// FIRST PASS: fetching the url with Accept of each content-type in
+// validContentTypes.
+
+// Collecting promises from the fetch() of ${url} with each Accept content-type.
+for (let contentType of validContentTypes) {
+    requestsPrm.push(fetch(url, {headers: {Accept: contentType}}));
 }
 
-let allReqs = Promise.all(requestsArray);
+// Promise code to collect the body text from each fetch() call and store it in
+// contentBodies.
+let allReqsPrms = Promise.all(requestsPrm);
 
-allReqs.then(responses => {
-    let textsArray = [];
+allReqsPrms.then(responses => {
+    // Collecting promises from each response.text() call.
+    let textsPrms = [];
     for (let response of responses) {
-        textsArray.push(response.text());
+        textsPrms.push(response.text());
     };
-    let allTexts = Promise.all(textsArray);
-    allTexts.then(texts => {
+
+    // Waiting on all response.text() promises.
+    let allTextsPrms = Promise.all(textsPrms);
+
+    allTextsPrms.then(texts => {
+        // Storing the response bodies in global data structure contentBodies.
         for (i = 0; i < 3; i++) {
-            let contentType = contentTypes[i];
+            let contentType = validContentTypes[i];
             let reqText = texts[i];
             contentBodies.set(contentType, reqText);
         }
-        allFetched = true;
+
+        // Setting global flag that setInterval() call below is looking for.
+        firstPassFetched = true;
     }).catch(error => {
         console.log(error);
     });
 });
 
-let intervalId = setInterval(() => {
-    if (allFetched) {
-        clearInterval(intervalId);
-        displayOutcomes();
+// Checks for completion of above promise code 10 times a second. Runs
+// displayOutcomes() for validContentTypes when it's done.
+let firstPassDisplayIntervalId = setInterval(() => {
+    if (firstPassFetched) {
+        clearInterval(firstPassDisplayIntervalId);
+        displayOutcomes(validContentTypes);
+        firstPassDisplayed = true;
     };
 }, 100);
 
-function displayResolveOutcomes() {
-    for (let [contentType, reqBody] of contentBodies.entries()) {
-        console.log(`requesting ${url} with Accept: ${contentType} header yielded this body text:`);
-        console.log(reqBody);
+// SECOND PASS: fetching the url with Accept: application/rainbows+unicorns
+let secondPassFetchIntervalId = setInterval(() => {
+    if (firstPassDisplayed) {
+        clearInterval(secondPassFetchIntervalId);
+        let ringerPrm = fetch(url, {headers: {Accept: "application/rainbows+unicorns"}});
+        // Collecting the body text of the response.
+        ringerPrm.then(response => {
+            let ringerTextPrm = response.text();
+            ringerTextPrm.then(text => {
+                contentBodies.set("application/rainbows+unicorns", text);
+                // Setting global flag that setInterval() below is looking for.
+                secondPassFetched = true;
+            }).catch(error => {
+                console.log(error);
+            });
+        });
+    }
+});
+
+// Checks for completion of above promise code 10 times a second. Runs
+// displayOutcomes() for application/rainbows+unicorns when it's done.
+let secondPassDisplayIntervalId = setInterval(() => {
+    if (secondPassFetched) {
+        clearInterval(secondPassDisplayIntervalId);
+        displayOutcomes(["application/rainbows+unicorns"]);
+    }
+}, 100);
+
+// Displays the body text from contentBodies for each content-type in its
+// argument.
+function displayOutcomes(contentTypes, error=false) {
+    for (let contentType of contentTypes) {
+        let replyBody = contentBodies.get(contentType);
+        console.log(`REQUESTING ${url} WITH Accept: ${contentType} HEADER YIELDED THIS BODY TEXT:`);
+        console.log(replyBody);
     }
 }
