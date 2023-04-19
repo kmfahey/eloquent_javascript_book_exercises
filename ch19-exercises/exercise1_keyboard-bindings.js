c0a5f6969f9337d705f7a15ae59bd0706c26ed33
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-04-19 15:19:43 -0700

    ch19, ex1 partially complete: keyboard tool picking

--- ch19-exercises/exercise1_keyboard-bindings.js
+++ ch19-exercises/exercise1_keyboard-bindings.js
@@ -6,11 +6,13 @@
  * by modifying the PixelEditor component. Add a tabIndex property of 0 to the
  * wrapping <div> element so that it can receive keyboard focus. Note that the
  * property corresponding to the tabindex attribute is called tabIndex, with a
- * capital I, and our elt function ex- pects property names. Register the key
+ * capital I, and our elt function expects property names. Register the key
  * event handlers directly on that element. This means you have to click, touch,
  * or tab to the application before you can interact with it with the keyboard.
  *
  * Remember that keyboard events have ctrlKey and metaKey (for the command key
  * on Mac) properties that you can use to see whether those keys are held down.
  */
+
+
 
